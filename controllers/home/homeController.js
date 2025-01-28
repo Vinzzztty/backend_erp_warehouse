358bc7fcdf3ff4aab8dffdaaecbd7953ad9152fa
Vinzzztty <kevinarnandes21@gmail.com>
2025-01-28 09:42:00 +0700

    add: total transaction

--- controllers/home/homeController.js
+++ controllers/home/homeController.js
@@ -1,6 +1,6 @@
 const db = require("../../models");
 
-exports.getTotals = async (req, res) => {
+exports.getTotalsMaster = async (req, res) => {
     try {
         // Count data for all entities
         const totals = {
@@ -30,6 +30,40 @@ exports.getTotals = async (req, res) => {
         });
     } catch (error) {
         console.error("Error fetching totals:", error);
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+// Retrieve totals for transactions
+exports.getTransactionTotals = async (req, res) => {
+    try {
+        // Count data for transaction-related entities
+        const totals = {
+            cxl: await db.Cxl.count(),
+            cxInvoice: await db.CxInvoice.count(),
+            cxQDetail: await db.CxQDetail.count(),
+            cxQuotation: await db.CxQuotation.count(),
+            gr: await db.Gr.count(),
+            grDetail: await db.GrDetail.count(),
+            lastMile: await db.LastMile.count(),
+            lastMileDetail: await db.LastMileDetail.count(),
+            pi: await db.Pi.count(),
+            piDetail: await db.PiDetail.count(),
+            piPayment: await db.PiPayment.count(),
+            purchaseOrder: await db.PurchaseOrder.count(),
+            purchaseOrderDetail: await db.PurchaseOrderDetail.count(),
+        };
+
+        // Respond with the aggregated totals
+        res.status(200).json({
+            success: true,
+            data: totals,
+        });
+    } catch (error) {
+        console.error("Error fetching transaction totals:", error);
         res.status(500).json({
             success: false,
             message: error.message,
--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require("../../controllers/home/homeController");
 
 // Define route for fetching totals
-router.get("/totals", homeController.getTotals);
+router.get("/total-masters", homeController.getTotalsMaster);
+router.get("/total-transactions", homeController.getTransactionTotals);
 
 module.exports = router;
